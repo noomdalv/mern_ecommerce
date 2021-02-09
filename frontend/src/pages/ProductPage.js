91f2aab7b5bcb89a7f67dc244ad49c80ca7ff806
noomdalv <vladlunasan@gmail.com>
2021-02-08 22:49:59 -0500

    added axios and useState for HomePage

--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
 
 const ProductPage = ({ match }) => {
+	const [products, setProducts] = useState([]);
 	const product = products.find((p) => p.id === match.params.id);
 	return (
 		<>
