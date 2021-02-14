dde2a9a3bc7e431d6798be4ef8c6545d1e0977c5
noomdalv <vladlunasan@gmail.com>
2021-02-14 16:56:55 -0500

    added users.js

---
+++ backend/data/users.js
@@ -0,0 +1,23 @@
+import bcrypt from "bcryptjs";
+
+const users = [
+	{
+		name: "Admin User",
+		email: "admin@example.com",
+		password: bcrypt.hashSync("abc123", 10),
+		isAdmin: true,
+	},
+	{
+		name: "Vlad Luna",
+		email: "vladluna@test.com",
+		password: bcrypt.hashSync("abc123", 10),
+	},
+	,
+	{
+		name: "John Doe",
+		email: "johndoe@test.com",
+		password: bcrypt.hashSync("abc123", 10),
+	},
+];
+
+export default users;
