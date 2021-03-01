a50aa1d1c9c63700ecd0986aa031fb1c2b7443bd
noomdalv <vladlunasan@gmail.com>
2021-02-28 21:31:31 -0500

    created productDetails action and reducers

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -4,12 +4,12 @@ import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
-import { listProducts } from "../actions/productActions"; 
+import { listProducts } from "../actions/productActions";
 
 const HomePage = () => {
 	const dispatch = useDispatch();
 
-	const productList = useSelector(state => state.productList) //from store.js combineReducers
+	const productList = useSelector((state) => state.productList); //from store.js combineReducers
 	const { loading, error, products } = productList;
 
 	useEffect(() => {
@@ -19,14 +19,19 @@ const HomePage = () => {
 	return (
 		<div>
 			<h1>Latest Products</h1>
-			{ loading ? <Loader /> : error ? <Message variant="danger">error</Message> : <Row>
-				{products.map((product) => (
-					<Col key={product.id} sm={12} md={6} lg={4} xl={3}>
-						<Product product={product} />
-					</Col>
-				))}
-			</Row>}
-			
+			{loading ? (
+				<Loader />
+			) : error ? (
+				<Message variant="danger">error</Message>
+			) : (
+				<Row>
+					{products.map((product) => (
+						<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+							<Product product={product} />
+						</Col>
+					))}
+				</Row>
+			)}
 		</div>
 	);
 };
