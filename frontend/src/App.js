ec4995524f92130387f9e3b1cc185de7b925bf4b
noomdalv <vladlunasan@gmail.com>
2021-03-14 18:30:36 -0500

    added /cart route and qty handler for product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
+import CartPage from "./pages/CartPage";
 
 const App = () => {
 	return (
@@ -12,8 +13,9 @@ const App = () => {
 			<Header />
 			<main className="py-3">
 				<Container>
-					<Route path="/products/:id" component={ProductPage} />
 					<Route path="/" component={HomePage} exact />
+					<Route path="/products/:id" component={ProductPage} />
+					<Route path="/cart/:id?" component={CartPage} />
 				</Container>
 			</main>
 			<Footer />
--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -2,12 +2,22 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { listProductDetails } from "../actions/productActions";
-import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import {
+	Row,
+	Col,
+	Image,
+	ListGroup,
+	Card,
+	Button,
+	Form,
+} from "react-bootstrap";
 import Rating from "../components/Rating";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 
-const ProductPage = ({ match }) => {
+const ProductPage = ({ history, match }) => {
+	const [qty, setQty] = useState(0);
+
 	const dispatch = useDispatch();
 
 	const productDetails = useSelector((state) => state.productDetails);
@@ -16,6 +26,10 @@ const ProductPage = ({ match }) => {
 	useEffect(() => {
 		dispatch(listProductDetails(match.params.id));
 	}, [dispatch, match]);
+
+	const addToCartHandler = () => {
+		history.push(`/cart/${match.params.id}?qty=${qty}`);
+	};
 
 	return (
 		<>
@@ -69,6 +83,7 @@ const ProductPage = ({ match }) => {
 								</ListGroup.Item>
 								<ListGroup.Item>
 									<Button
+										onClick={addToCartHandler}
 										className="btn-block"
 										type="button"
 										disabled={product.countInStock === 0}
@@ -76,6 +91,26 @@ const ProductPage = ({ match }) => {
 										Add To Cart
 									</Button>
 								</ListGroup.Item>
+								{product.countInStock > 0 && (
+									<ListGroup.Item>
+										<Row>
+											<Col>Qty</Col>
+											<Col>
+												<Form.Control
+													as="select"
+													value={qty}
+													onChange={(e) => setQty(e.target.value)}
+												>
+													{[...Array(product.countInStock).keys()].map((x) => (
+														<option key={x + 1} value={x + 1}>
+															{x + 1}
+														</option>
+													))}
+												</Form.Control>
+											</Col>
+										</Row>
+									</ListGroup.Item>
+								)}
 							</ListGroup>
 						</Card>
 					</Col>
