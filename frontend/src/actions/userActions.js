c8c54bd597c7d8b12b659a8c509d276d334db4c8
noomdalv <vladlunasan@gmail.com>
2021-04-05 23:58:41 -0500

    created login page and workflow

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -2,9 +2,10 @@ import axios from "axios";
 import {
 	USER_LOGIN_REQUEST,
 	USER_LOGIN_SUCCESS,
+	USER_LOGIN_FAIL,
 } from "../constants/userConstants";
 
-export const login = ({ email, password } = async (dispatch) => {
+export const login = (email, password) => async (dispatch) => {
 	try {
 		dispatch({
 			type: USER_LOGIN_REQUEST,
@@ -37,4 +38,4 @@ export const login = ({ email, password } = async (dispatch) => {
 					: error.message,
 		});
 	}
-});
+};
