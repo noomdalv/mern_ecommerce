169440a251efae0f204a0f34aa9b9d29a99d20bd
noomdalv <vladlunasan@gmail.com>
2021-04-21 21:53:34 -0500

    created CheckoutSteps component

--- frontend/src/pages/ShippingPage.js
+++ frontend/src/pages/ShippingPage.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from "react";
 import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer";
+import CheckoutSteps from "../components/CheckoutSteps";
 import { saveShippingAddress } from "../actions/cartActions";
 
-const CheckoutPage = ({ history }) => {
+const ShippingPage = ({ history }) => {
 	const cart = useSelector((state) => state.cart);
 	const { shippingAddress } = cart;
 
@@ -23,6 +24,7 @@ const CheckoutPage = ({ history }) => {
 
 	return (
 		<FormContainer>
+			<CheckoutSteps step1 step2 />
 			<h1>Shipping</h1>
 			<Form onSubmit={submitHandler}>
 				<Form.Group controlId="address">
@@ -77,4 +79,4 @@ const CheckoutPage = ({ history }) => {
 	);
 };
 
-export default CheckoutPage;
+export default ShippingPage;
