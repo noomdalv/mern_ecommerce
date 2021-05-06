c97d8047e9be053e8d6110ad6f0d637b64789c45
noomdalv <vladlunasan@gmail.com>
2021-05-05 23:12:48 -0500

    imported paypal sdk script dinamically in OrderPage

--- frontend/src/pages/OrderPage.js
+++ frontend/src/pages/OrderPage.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import { Button, Row, Col, ListGroup, Image, Card } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
@@ -9,12 +10,28 @@ import { getOrderDetails } from "../actions/orderActions";
 const OrderPage = ({ match }) => {
 	const dispatch = useDispatch();
 
+	const [sdkReady, setSdkReady] = useState(false);
+
 	const orderId = match.params.id;
 
 	const orderDetails = useSelector((state) => state.orderDetails);
 	const { order, loading, error } = orderDetails;
 
 	useEffect(() => {
+		const addPayPalScript = async () => {
+			const { data: clientId } = await axios.get("/api/config/paypal");
+			const script = document.createElement("script");
+			script.type = "text/javascript";
+			script.async = true;
+			script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`;
+			script.onload = () => {
+				setSdkReady(true);
+			};
+			document.body.appendChild(script);
+		};
+
+		addPayPalScript();
+
 		dispatch(getOrderDetails(orderId));
 	}, [dispatch, orderId]);
 
