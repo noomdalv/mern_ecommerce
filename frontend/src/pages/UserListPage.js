93045212b31b89808a3fcd1a2bc2a5d40c9cd8e7
noomdalv <vladlunasan@gmail.com>
2021-05-12 00:30:43 -0500

    added deleteUser action/reducer and handler to UserListPage

--- frontend/src/pages/UserListPage.js
+++ frontend/src/pages/UserListPage.js
@@ -4,7 +4,7 @@ import { Table, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
-import { getUserList } from "../actions/userActions";
+import { getUserList, deleteUser } from "../actions/userActions";
 
 const UserListPage = ({ history }) => {
 	const dispatch = useDispatch();
@@ -15,16 +15,21 @@ const UserListPage = ({ history }) => {
 	const userLogin = useSelector((state) => state.userLogin);
 	const { userInfo } = userLogin;
 
+	const userDelete = useSelector((state) => state.userDelete);
+	const { success: successDelete } = userDelete;
+
 	useEffect(() => {
 		if (userInfo && userInfo.isAdmin) {
 			dispatch(getUserList());
 		} else {
 			history.push("/login");
 		}
-	}, [dispatch, userInfo, history]);
+	}, [dispatch, userInfo, history, successDelete]);
 
 	const deleteHandler = (id) => {
-		console.log("delete");
+		if (window.confirm("Are you sure?")) {
+			dispatch(deleteUser(id));
+		}
 	};
 
 	return (
